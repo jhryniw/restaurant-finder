47ff21b0d484da02d4b26f54377465556297cf8a
jhryniw <jhryniw@gmail.com>
2017-10-26 08:33:06 -0600

    joy cursor

--- restaurant-finder.cpp
+++ restaurant-finder.cpp
@@ -1,8 +1,114 @@
 /**
  * @file restaurant-finder.cpp
  *
- * Main file
+ * @author James Hryniw 1431912
+ * @author Songhui Zhang 1499982
  */
+
+ #include <Arduino.h>
+ #include "Adafruit_ILI9341.h" // Hardware-specific library
+ #include <SPI.h>
+ #include <SD.h>
+ #include "lcd_image.h"
+ #include "joy.h"
+
+ #define TFT_DC 9
+ #define TFT_CS 10
+ #define SD_CS  6
+
+ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+ #define DISPLAY_WIDTH      240
+ #define DISPLAY_HEIGHT     320
+ #define DISPLAY_BAR_HEIGHT  48
+ #define YEG_SIZE          2048
+ #define CURSOR_SIZE         10
+
+ lcd_image_t yegImage = { "yeg-big.lcd" , YEG_SIZE, YEG_SIZE };
+
+ int yegMiddleX, yegMiddleY;
+ int cursorX, cursorY;
+
+ /* Redraw the cursor if it has moved */
+ void redrawCursor(joy_state_t joy_state) {
+
+     // i.e the joystick is not in the center (will move)
+     if (joy_state.direction) {
+
+         // Draw over the old cursor postition with the map
+         lcd_image_draw(&yegImage, &tft,
+                        // coordinates in top left
+                        yegMiddleX + (cursorX - CURSOR_SIZE/2), yegMiddleY + (cursorY - CURSOR_SIZE / 2),
+                        // start coordinates on display
+                        cursorX - CURSOR_SIZE/2, cursorY - CURSOR_SIZE/2,
+                        CURSOR_SIZE, CURSOR_SIZE);
+
+         // Move the cursor, note that cursorX and cursorY are mutated by this function
+         moveCursor(cursorX, cursorY, joy_state);
+
+         // Constrain the cursor position to the screen
+         cursorX = constrain(cursorX, CURSOR_SIZE/2, DISPLAY_HEIGHT - DISPLAY_BAR_HEIGHT - CURSOR_SIZE/2);
+         cursorY = constrain(cursorY, CURSOR_SIZE/2, DISPLAY_WIDTH - CURSOR_SIZE/2);
+
+         // Draw the cursor at the new position
+         tft.fillRect(cursorX - CURSOR_SIZE/2, cursorY - CURSOR_SIZE / 2,
+                      CURSOR_SIZE, CURSOR_SIZE, ILI9341_RED);
+     }
+ }
+
+ void setup() {
+     init();
+
+     Serial.begin(9600);
+     tft.begin();
+
+     Serial.print("Initializing SD card...");
+     if (!SD.begin(SD_CS)) {
+         Serial.print("failed! Is it inserted properly?");
+         while(true) {}
+     }
+
+     tft.setRotation(3);
+
+     tft.fillScreen(0);
+
+     yegMiddleX = YEG_SIZE/2 - (DISPLAY_HEIGHT - DISPLAY_BAR_HEIGHT)/2;
+     yegMiddleY = YEG_SIZE/2 - DISPLAY_WIDTH/2;
+
+     lcd_image_draw(&yegImage, &tft,
+                    // coordinates in top left
+                    yegMiddleX, yegMiddleY,
+                    // start coordinates on display
+                    0, 0,
+                    // Display width and height
+                    DISPLAY_HEIGHT - DISPLAY_BAR_HEIGHT, DISPLAY_WIDTH);
+
+     // Cursor starts in the center of the screen
+     cursorX = DISPLAY_WIDTH / 2;
+     cursorY = (DISPLAY_HEIGHT - DISPLAY_BAR_HEIGHT) / 2;
+
+     tft.fillRect(cursorX - CURSOR_SIZE/2, cursorY - CURSOR_SIZE / 2,
+                  CURSOR_SIZE, CURSOR_SIZE, ILI9341_RED);
+
+     initJoy();
+ }
+
+ int main() {
+     setup();
+
+     while(true) {
+         // Read the joystick state
+         joy_state_t joy_state = readJoy();
+
+         // Redraw the cursor
+         redrawCursor(joy_state);
+
+         delay(10);
+     }
+
+     Serial.end();
+     return 0;
+ }
 
 void setup() {
     init();
